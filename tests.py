ac3f445c5e6f7587b8f39432bcaad4063609952f
Iaroslav Baslin <wertout77@gmail.com>
2022-11-12 17:06:51 +0300

    Refactoring

--- tests.py
+++ tests.py
@@ -4,12 +4,19 @@ from algorithms import gauss_elimination as gauel
 from algorithms import seidel
 from algorithms import tridiagonal_matrix_algorithm as trimatal
 
+from equations.tridiagonal_sample import matrix as tridiag_m, vec as tridiag_v, correct_ans as tridiag_ans
+
 from equations.sample1 import matrix as coef_mat1, vec as vec1, correct_ans as ans1, corr_coef as coef1
 from equations.sample2 import matrix as coef_mat2, vec as vec2, correct_ans as ans2, corr_coef as coef2
 from equations.sample3 import matrix as coef_mat3, vec as vec3, correct_ans as ans3, corr_coef as coef3
 
 
-class TestSoLAEMethods(unittest.TestCase):
+class TestGausMethod(unittest.TestCase):
+    """Test cases for gaussian method."""
+
+
+class TestGauelMethod(unittest.TestCase):
+    """Test cases for gauss elimination method."""
 
     def test_gauss_elimination_method_1(self):
         test = gauel.gauss_elimination(coef_mat1, vec1)
@@ -23,17 +30,14 @@ class TestSoLAEMethods(unittest.TestCase):
         test = gauel.gauss_elimination(coef_mat3, vec3)
         self.assertEqual(ans3, test)
 
-    def test_seidel_method_1(self):
-        test = seidel.Zeydel(coef_mat1, vec1, coef1)
-        self.assertEqual(ans1, test)
 
-    def test_seidel_method_2(self):
-        test = seidel.Zeydel(coef_mat2, vec2, coef2)
-        self.assertEqual(ans2, test)
+class TestGauPivotingMethod(unittest.TestCase):
+    """Test cases for gauss with pivoting."""
+    pass
 
-    def test_seidel_method_3(self):
-        test = seidel.Zeydel(coef_mat3, vec3, coef3)
-        self.assertEqual(ans3, test)
+
+class TestTridiagonalMethod(unittest.TestCase):
+    """Test cases for tridiagonal matrix method."""
 
     def test_tridiagonal_matrix_algorithm_1(self):
         test = trimatal.backward(coef_mat1, vec1)
@@ -47,3 +51,32 @@ class TestSoLAEMethods(unittest.TestCase):
         test = trimatal.backward(coef_mat3, vec3)
         self.assertEqual(ans3, test)
 
+    def test_tridiagonal_matrix_algorithm_4(self):
+        test = trimatal.backward(tridiag_m, tridiag_v)
+        self.assertEqual(tridiag_ans, test)
+
+
+class TestLUDecompMethod(unittest.TestCase):
+    """Test cases for LU decomposition method."""
+    pass
+
+
+class TestSimpleIterMethod(unittest.TestCase):
+    """Test cases for simple iteration method."""
+    pass
+
+
+class TestSeidelMethod(unittest.TestCase):
+    """Test cases for Seidel method."""
+
+    def test_seidel_method_1(self):
+        test = seidel.Zeydel(coef_mat1, vec1, coef1)
+        self.assertEqual(ans1, test)
+
+    def test_seidel_method_2(self):
+        test = seidel.Zeydel(coef_mat2, vec2, coef2)
+        self.assertEqual(ans2, test)
+
+    def test_seidel_method_3(self):
+        test = seidel.Zeydel(coef_mat3, vec3, coef3)
+        self.assertEqual(ans3, test)
