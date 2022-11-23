347ad1341a0db43140dc28e833845a9896690544
DeniRadionov <deniradionov190202@gmail.com>
2022-11-23 20:20:25 +0300

    add Deni

--- algorithms/gauss_leader.py
+++ algorithms/gauss_leader.py
@@ -2,36 +2,41 @@ import numpy as np
 
 
 def bubble_max_row(m, col):
+    save = m
     max_element = m[col][col]
     max_row = col
     for i in range(col + 1, len(m)):
         if abs(m[i][col]) > abs(max_element):
             max_element = m[i][col]
             max_row = i
+
     if max_row != col:
-        m[col], m[max_row] = m[max_row], m[col]
+        m[[max_row, col]] = m[[col, max_row]]
 
 
 def solve(m, X):
-    nn = len(X)
-    m = np.hstack((m, X))
+    m[0][0] *= 1.0
+    sa = len(X)
+    mas = []
     n = len(m)
+    for i in range(sa):
+        mas.append([0] * 1)
+        mas[i][0] = X[i]
+    m = np.hstack((m, mas))
 
     for k in range(n - 1):
         bubble_max_row(m, k)
         for i in range(k + 1, n):
             div = m[i][k] / m[k][k]
 
-            m[i][n] -= div * m[k][n]
+            m[i][-1] -= div * m[k][-1]
 
             for j in range(k, n):
                 m[i][j] -= div * m[k][j]
 
-
     if is_singular(m):
         print('The system has infinite number of answers.')
         return
-
 
     x = [0 for i in range(n)]
     for k in range(n - 1, -1, -1):
